542d8d4dbd593ea30e2b4549e3b0dabe0a01673f
Jonathas <jonathasthp@gmail.com>
2025-01-23 22:54:40 +0000

    mend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Você deu um empurrão gigante pra um novo profissional, uma nova vida, Muito obrigado.</h1>
+    return <h1>Você deu um empurrão gigante pra um novo profissional, uma nova vida, Muito obrigado.</h1>       
 }
 
 export default Home;
